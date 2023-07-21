edf86d0fdc88f8a85cbc27d2091d49bf340b7c85
ishaKanthale <110197912+ishaKanthale@users.noreply.github.com>
2023-07-20 22:52:41 -0700

    Create allocateMemory.c

---
+++ Chapter-11 (Dynamic Memory Allocation)/allocateMemory.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+	int n;
+	printf("Enter number: ");
+	scanf("%d", &n);
+	
+	int *ptr;
+	ptr = (int *) calloc(n, sizeof(int));
+
+	for(int i=0; i<n; i++){
+		printf("%d\n", ptr[i]);
+	}
+	
+	free(ptr);
+	
+	return 0;
+}
